b7e6638a2c73be2bb07efa78485323b6c4be1301
Shadab Khan <shadabkhan@Shadabs-Mac-mini.local>
2018-01-22 12:20:05 +0530

    new commit

---
+++ new/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  new
+//
+//  Created by Shadab Khan on 1/22/18.
+//  Copyright © 2018 Shadab Khan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+   
+
+
+}
+
